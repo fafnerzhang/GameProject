08ecc4bd1543f3737e697a9df070617d79e8710d
fafnerzhang <43285005+fafnerzhang@users.noreply.github.com>
2019-03-15 20:29:01 +0800

    3/15_addMenu
    加menu

---
+++ Assets/Script/URLlinked.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLlinked : MonoBehaviour {
+    
+    public void ToURL()
+    {
+        Application.OpenURL("https://docs.unity3d.com/ScriptReference/Application.OpenURL.html");
+    }
+}
